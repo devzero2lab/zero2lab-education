14d1bb675c15c59945a90c4c2e1f1b3e685ddf7d
anupaisuru <anupaisuru2002@gmail.com>
2024-12-30 15:00:44 +0530

    create course routes

---
+++ app/api/courses/[id]/route.js
@@ -0,0 +1,63 @@
+import connectMongoDB from "@/lib/db";
+import { Course } from "@/models/course";
+import { NextResponse } from "next/server";
+
+//get single course
+export async function GET(request, { params }) {
+  try {
+    const { id } = params;
+    await connectMongoDB();
+    const course = await Course.findById(id);
+    if (!course) {
+      return NextResponse.json({ error: "Course not found" }, { status: 404 });
+    }
+    return NextResponse.json({ course }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+//update course
+export async function PUT(request, { params }) {
+  try {
+    const { id } = params;
+    const updatedData = await request.json();
+    await connectMongoDB();
+    const updatedCourse = await Course.findByIdAndUpdate(id, updatedData, {
+      new: true,
+    });
+    if (!updatedCourse) {
+      return NextResponse.json({ error: "Course not found" }, { status: 404 });
+    }
+    return NextResponse.json(
+      { message: "Course updated", updatedCourse },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+//delete course
+export async function DELETE(request, { params }) {
+  try {
+    const { id } = params;
+    if (!id) {
+      return NextResponse.json(
+        { error: "Course ID is required" },
+        { status: 400 }
+      );
+    }
+    await connectMongoDB();
+    const deletedCourse = await Course.findByIdAndDelete(id);
+    if (!deletedCourse) {
+      return NextResponse.json({ error: "Course not found" }, { status: 404 });
+    }
+    return NextResponse.json(
+      { message: "Course deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
---
+++ app/api/courses/route.js
@@ -0,0 +1,26 @@
+import connectMongoDB from "@/lib/db";
+import { Course } from "@/models/course";
+import { NextResponse } from "next/server";
+
+//create course
+export async function POST(request) {
+    try {
+      const courseData = await request.json();
+      await connectMongoDB();
+      const newCourse = await Course.create(courseData);
+      return NextResponse.json({ message: "Course created", newCourse }, { status: 201 });
+    } catch (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+  }
+
+//get all courses
+export async function GET() {
+    try {
+      await connectMongoDB();
+      const courses = await Course.find();
+      return NextResponse.json({ courses }, { status: 200 });
+    } catch (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+  }
