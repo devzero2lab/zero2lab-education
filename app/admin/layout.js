90989d6de74514184f9b49b617175794d07fa1de
anupaisuru <anupaisuru2002@gmail.com>
2025-01-06 21:57:14 +0530

    update admin layout with loading protection

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,22 +1,26 @@
 "use client";
 import { useAuth, useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function AdminLayout({ children }) {
   const { isLoaded, isSignedIn } = useAuth();
   const { user } = useUser();
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Redirect non-admin users or unauthenticated users
-    if (isLoaded && (!isSignedIn || user.publicMetadata.role !== "admin")) {
-      router.push("/"); // Redirect to home page
+    if (isLoaded) {
+      if (!isSignedIn || user.publicMetadata.role !== "admin") {
+        router.push("/");
+      } else {
+        setLoading(false); // Stop loading once authorized
+      }
     }
   }, [isLoaded, isSignedIn, user, router]);
 
-  if (!isLoaded || !isSignedIn) {
-    return <div>Loading...</div>;
+  if (loading) {
+    return <div>Loading...</div>; // Show a loading screen
   }
 
   return (
