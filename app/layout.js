e141b2f62b372daad1283af06c297a7e13bd785b
devzero2lab <dev.zero2lab@gmail.com>
2025-01-08 12:07:34 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ClerkProvider>
-          <Header />
+          <Header /> 
           <Toaster position="top-right" richColors />
           {children}
           <Footer />
