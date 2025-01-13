a0525c12f19ba93a816a780c8219c358aade406c
anupaisuru <anupaisuru2002@gmail.com>
2025-01-13 18:19:38 +0530

    update sidebar with not enrolled users

--- app/admin/Sidebar.jsx
+++ app/admin/Sidebar.jsx
@@ -17,6 +17,7 @@ function Sidebar() {
           { menu: "courses", path: "/admin/course" },
           { menu: "Pending Access", path: "/admin/pending" },
           { menu: "Approved Access", path: "/admin/approved" },
+          { menu: "Not Enrolled users", path: "/admin/users" },
         ].map(({ menu, path }) => (
           <Link key={menu} href={path}>
             <li
