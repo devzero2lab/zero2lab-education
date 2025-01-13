0d0ab874cf2dde740cfc90ab1167fb0efd5ee92f
anupaisuru <anupaisuru2002@gmail.com>
2025-01-13 22:31:35 +0530

    done not enrolled users feature

---
+++ app/api/users/route.js
@@ -0,0 +1,26 @@
+import connectMongoDB from "@/lib/db";
+import { User } from "@/models/user";
+import { UserCourse } from "@/models/userCourse";
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+  try {
+    await connectMongoDB();
+
+    // Fetch all user IDs that are enrolled in any course
+    const enrolledUserIds = await UserCourse.find().distinct("userId");
+
+    // Fetch all users who are not in the enrolled list
+    const notEnrolledUsers = await User.find({
+      clerkId: { $nin: enrolledUserIds }, // Exclude users with clerkId in enrolledUserIds
+    });
+
+    return NextResponse.json({ notEnrolledUsers }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching not enrolled users:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error", message: error.message },
+      { status: 500 }
+    );
+  }
+}
