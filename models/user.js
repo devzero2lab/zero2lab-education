012caf4cbdca67d754f50fcef3a0529d034835bf
anupaisuru <anupaisuru2002@gmail.com>
2025-01-13 21:45:49 +0530

    update user model and user create function

--- models/user.js
+++ models/user.js
@@ -5,10 +5,12 @@ const userSchema = new mongoose.Schema(
     clerkId: {
       type: String,
       required: true,
+      unique: true,
     },
     email: {
       type: String,
       required: true,
+      unique: true,
     },
     firstName: {
       type: String,
