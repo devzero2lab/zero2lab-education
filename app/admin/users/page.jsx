17886a2d013c9f8d9f5239ad9a25764c7b46b7ec
anupaisuru <anupaisuru2002@gmail.com>
2025-01-14 12:43:53 +0530

    create user delete option

--- app/admin/users/page.jsx
+++ app/admin/users/page.jsx
@@ -1,7 +1,9 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Table } from "antd";
+import { Table, Button } from "antd";
 import axios from "axios";
+import { toast } from "sonner";
+import Swal from "sweetalert2";
 
 const UsersPage = () => {
   const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
@@ -41,11 +43,54 @@ const UsersPage = () => {
       dataIndex: "lastName",
       key: "lastName",
     },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Button
+          type="danger"
+          onClick={() => handleDelete(record)}
+          className="font-semibold text-white bg-red-500 border-red-500 hover:bg-red-600"
+        >
+          Delete
+        </Button>
+      ),
+    },
   ];
 
   if (loading) {
     return <div>Loading...</div>;
   }
+
+  const handleDelete = async (record) => {
+    // Show confirmation dialog using SweetAlert2
+    const result = await Swal.fire({
+      title: "Are you sure?",
+      text: `Do you want to delete user ${record.email} ?`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Yes, delete it!",
+    });
+
+    if (result.isConfirmed) {
+      try {
+        await axios.delete(`${apiUrl}/api/users/${record._id}`);
+        toast.success(`User ${record.email} deleted successfully.`);
+        setData((prevData) =>
+          prevData.filter((user) => user._id !== record._id)
+        );
+      } catch (error) {
+        toast.error(`Failed to delete user ${record.email}.`);
+        Swal.fire(
+          "Error!",
+          "An error occurred while deleting the user.",
+          "error"
+        );
+      }
+    }
+  };
 
   return (
     <div>
