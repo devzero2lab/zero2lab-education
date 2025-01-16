d5c28d6e5bd07033520f5af86fe366040e4e6f16
anupaisuru <anupaisuru2002@gmail.com>
2025-01-16 21:15:12 +0530

    update footer,update admin sidebar , and create feedbacks page in admin side

--- app/admin/Sidebar.jsx
+++ app/admin/Sidebar.jsx
@@ -1,32 +1,30 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function Sidebar() {
-  const [activeMenu, setActiveMenu] = useState("courses"); // Track the active menu item
+  const pathname = usePathname();
 
-  const handleMenuClick = (menu) => {
-    setActiveMenu(menu); // Update the active menu
-  };
+  const menuItems = [
+    { menu: "courses", path: "/admin/course" },
+    { menu: "Pending Access", path: "/admin/pending" },
+    { menu: "Approved Access", path: "/admin/approved" },
+    { menu: "Not Enrolled users", path: "/admin/notenroll" },
+    { menu: "Feedbacks", path: "/admin/feedbacks" },
+  ];
 
   return (
     <div className="w-64 h-screen p-4 text-white bg-gray-800">
       <ul className="space-y-4">
-        {/* Add more links as needed */}
-        {[
-          { menu: "courses", path: "/admin/course" },
-          { menu: "Pending Access", path: "/admin/pending" },
-          { menu: "Approved Access", path: "/admin/approved" },
-          { menu: "Not Enrolled users", path: "/admin/notenroll" },
-        ].map(({ menu, path }) => (
+        {menuItems.map(({ menu, path }) => (
           <Link key={menu} href={path}>
             <li
               className={`cursor-pointer p-4 rounded-md text-center my-4 ${
-                activeMenu === menu
+                pathname === path
                   ? "bg-blue-500 shadow-lg"
                   : "bg-gray-700 hover:bg-gray-600"
               }`}
-              onClick={() => handleMenuClick(menu)}
             >
               <span>{menu.charAt(0).toUpperCase() + menu.slice(1)}</span>
             </li>
