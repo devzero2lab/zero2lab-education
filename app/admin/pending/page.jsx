96f89440a70c43c490fe31b1623b644ae1e07830
anupaisuru <anupaisuru2002@gmail.com>
2025-01-18 23:15:02 +0530

    add spinning animation for admin dashboard

--- app/admin/pending/page.jsx
+++ app/admin/pending/page.jsx
@@ -2,12 +2,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import PendingList from "./PendingList";
+import { Spin } from "antd";
 
 function PendingPage() {
   const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
   const [pendingCourses, setPendingCourses] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetchPendingCourses = async () => {
+    setLoading(true);
     try {
       const response = await axios.get(`${apiUrl}/api/usercourses`, {
         params: { status: "Pending" },
@@ -15,6 +18,8 @@ function PendingPage() {
       setPendingCourses(response.data.userCourses || []);
     } catch (error) {
       console.error("Error fetching pending courses:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -25,10 +30,12 @@ function PendingPage() {
   return (
     <div className="p-6">
       <h1 className="mb-4 text-2xl font-bold">Pending Courses Access</h1>
-      <PendingList
-        courses={pendingCourses}
-        fetchPendingCourses={fetchPendingCourses}
-      />
+      <Spin spinning={loading}>
+        <PendingList
+          courses={pendingCourses}
+          fetchPendingCourses={fetchPendingCourses}
+        />
+      </Spin>
     </div>
   );
 }
