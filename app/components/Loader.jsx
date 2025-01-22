09c9f2a389205989f888b97cceb1046f35c2efaf
devzero2lab <dev.zero2lab@gmail.com>
2025-01-22 19:38:48 +0530

    redeploy hero session update 4

--- app/components/Loader.jsx
+++ app/components/Loader.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-const Loader = () => {
+const Loader = () => { 
   const [isLoading, setIsLoading] = useState(true);
 
   if (!isLoading) return null;
