11f7d098717418953710fe2bed6effeac4c84380
devzero2lab <dev.zero2lab@gmail.com>
2025-02-01 17:26:51 +0530

    certifacate update

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ function Home() {
   return (  
     <div className="mt-12 bg-[#ffffff]"> 
        
-      {/* hero section */}
+      {/* hero section */} 
       <Hero />
       {/* TechStacks */}
       {/* <TechStackIcons />  */}
